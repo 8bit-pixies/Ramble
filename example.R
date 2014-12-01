8d620f3d90a4d0a459058c1caefaa4ad9709614c
Chapman Siu <chapm0n.siu@gmail.com>
2014-12-01 15:54:32 +1100

    Update example.R
    remove dependency on `do`

--- example.R
+++ example.R
@@ -5,41 +5,33 @@
 #' factor :: = (expr) | nat
 #' nat :: = 0 | 1 | 2 | ...
 
-expr <- do(f=term, 
-           function(f, leftover_){
-             return(
-               (do(s=symbol("+"),
-                   t=expr,
-                   function(s,t) {
-                     print(unlist(c(f,s,t)))
-                     return(unlist(c(f,s,t)))
-                   })
-                %+++% return(f)) (leftover_)
-             )
-           })
+expr <- ((term %then% 
+            symbol("+") %then%
+            expr %using% function(x) {
+              print(unlist(c(x)))
+              return(paste(unlist(c(x)), collapse=""))
+            }) %alt% term)
 
-term <- do(f=factor, 
-           function(f, leftover_){
-             return(
-               (do(s=symbol("*"),
-                   t=term,
-                   function(s,t) {
-                     print(unlist(c(f,s,t)))
-                     return(unlist(c(f,s,t)))
-                   })
-                %+++% return(f)) (leftover_)
-             )
-           })
 
-factor <- (do(sl=symbol("("),
-                   e=expr,
-                   sr=symbol(")"),
-              function(sl, e, sr) {
-                print(unlist(c(sl, e, sr)))
-                return(unlist(c(sl, e, sr)))
-              })
-           %+++% natural ())
+term <- ((factor %then% 
+             symbol("*") %then%
+             term %using% function(x) {
+               print(unlist(c(x))[c(1,3)])
+               return(paste(unlist(c(x)), collapse=""))
+             }) %alt% factor)
 
+factor <- ((
+    symbol("(") %then%
+      expr %then%
+      symbol(")") %using% 
+      function(x){
+        print(unlist(c(x)))
+        return(paste(unlist(c(x)), collapse=""))
+        })
+    %alt% natural())
+
+expr("1+2+3+4+5+6")
+expr("1+(2+3)*4+5+6")
 factor("(1)")
 factor("1")
 expr("1+(2*2)")
