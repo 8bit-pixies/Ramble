3d01f9ba4e497c9c211f153b52f03c474fa75493
Chapman Siu <chapm0n.siu@gmail.com>
2014-12-08 06:01:54 +0000

    add exmaplepl0

---
+++ example-pl0.R
@@ -0,0 +1,141 @@
+#' PL/0 language using Ramble
+#' 
+#' [PL/0](http://en.wikipedia.org/wiki/PL/0) is a simple language which is
+#' used for educational purposes. Here we will provide a sample implementation
+#' based on it.
+#' 
+#' ## EBNF grammar
+#' 
+#' program = block "." .
+#' 
+#' block = [ "const" ident "=" number {"," ident "=" number} ";"]
+#' [ "var" ident {"," ident} ";"]
+#' { "procedure" ident ";" block ";" } statement .
+#' 
+#' statement = [ ident ":=" expression | "call" ident 
+#'               | "?" ident | "!" expression 
+#'               | "begin" statement {";" statement } "end" 
+#'               | "if" condition "then" statement 
+#'               | "while" condition "do" statement ].
+#' 
+#' condition = "odd" expression |
+#'   expression ("="|"#"|"<"|"<="|">"|">=") expression .
+#' 
+#' expression = [ "+"|"-"] term { ("+"|"-") term}.
+#' 
+#' term = factor {("*"|"/") factor}.
+#' 
+#' factor = ident | number | "(" expression ")".
+
+#' for the purposes of this example, all the assignments will be of the form
+#' `assign(x, value, env=PL0)`, this will then assign the variable to 
+#' PL0 environment, which we will define.
+
+PL0 <- new.env()
+
+# statement("if 1 > 2 then var1 := 3")
+# statement("test := 1+2+3")
+# statement("call test")
+statement <- (((identifier() %then% token(String(":=")) %then% expr)
+               %using% function(stateVar) {
+                 if (stateVar[[2]] == ":=") {
+                   assign(stateVar[[1]], stateVar[[3]], env=PL0)
+                 }
+                 return(stateVar)
+               })
+            %alt% (symbol("!") %then% identifier() 
+                   %using% function(stateVar) {
+                     # this calls a defined function (procedure)
+                     print(get(stateVar[[2]], envir = PL0))
+                     return(stateVar)
+                   })
+
+             %alt% (token(String("if")) %then% condition %then% token(String("then")) 
+                    %then% statement %using% function(x) {
+                      if(x[[2]]) {
+                        return(x[[4]])
+                      }
+                      else {
+                        return(x)
+                      }
+                    })
+             %alt% (token(String("while")) %then% condition %then% token(String("do")) 
+                    %then% statement)
+             %alt% (token(String("begin")) %then% ((statement %then% symbol(";") %then% statement) %alt% statement)
+                    %then% token(String("end")))
+             %alt% (token(String("call")) %then% identifier())
+            )
+
+condition <- (expr %then% (token(String("<="))
+                           %alt% token(String(">="))
+                           %alt% symbol("=") 
+                           %alt% symbol("<")
+                           %alt% symbol(">")) 
+                   %then% expr
+                   %using% function(x) {
+                     print(unlist(c(x)))
+                     #' try to return a boolean otherwise return unlist vector
+                     bool <- unlist(c(x))                     
+                     if (bool[2] == "<") {
+                       try(bool1 <- as.numeric(bool[1]) < as.numeric(bool[3]), silent=TRUE)
+                       return(if (is.na(bool1)) bool else bool1)
+                     }
+                     else if (bool[2] == ">") {
+                       try(bool1 <- as.numeric(bool[1]) > as.numeric(bool[3]), silent=TRUE)
+                       return(if (is.na(bool1)) bool else bool1)
+                     }
+                     else if (bool[2] == "=") {
+                       try(bool1 <- as.numeric(bool[1]) == as.numeric(bool[3]), silent=TRUE)
+                       return(if (is.na(bool1)) bool else bool1)
+                     }
+                     else if (bool[2] == "<=") {
+                       try(bool1 <- as.numeric(bool[1]) <= as.numeric(bool[3]), silent=TRUE)
+                       return(if (is.na(bool1)) bool else bool1)
+                     }
+                     else if (bool[2] == ">=") {
+                       try(bool1 <- as.numeric(bool[1]) >= as.numeric(bool[3]), silent=TRUE)
+                       return(if (is.na(bool1)) bool else bool1)
+                     }
+                     else {
+                       warning("boolean symbol should have matched, please check PL\\0 implementation")
+                       return(bool)
+                     }
+                   })
+
+expr <- ((term %then% 
+            symbol("+") %then%
+            expr %using% function(x) {
+              #print(unlist(c(x)))
+              return(sum(as.numeric(unlist(c(x))[c(1,3)])))
+            }) %alt% 
+           (term %then% 
+              symbol("-") %then%
+              expr %using% function(x) {
+                #print(unlist(c(x)))
+                return(Reduce("-", as.numeric(unlist(c(x))[c(1,3)])))
+              }) %alt% term)
+
+term <- ((factor %then% 
+            symbol("*") %then%
+            term %using% function(x) {
+              #print(unlist(c(x)))
+              return(prod(as.numeric(unlist(c(x))[c(1,3)])))
+            }) %alt% 
+           (factor %then% 
+              symbol("/") %then%
+              term %using% function(x) {
+                #print(unlist(c(x)))
+                return(Reduce("/", as.numeric(unlist(c(x))[c(1,3)])))
+              }) %alt% factor)
+
+factor <- ((symbol("(") %then%
+              expr %then%
+              symbol(")") %using% function(x){
+                #print(unlist(c(x)))
+                return(as.numeric(unlist(c(x))[2]))
+              }) %alt% (natural() %using% function(x) {
+                as.numeric(x)
+              })
+                 %alt% identifier())
+
+
